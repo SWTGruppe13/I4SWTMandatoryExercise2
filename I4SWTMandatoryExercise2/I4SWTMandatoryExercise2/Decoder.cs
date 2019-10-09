cc965f6ed0ed69f7d763038f1f472b519ff5986d
Jens Krog Lodberg <jkrog1306@gmail.com>
2019-10-09 14:33:48 +0200

    Decodes string into FlightData class

---
+++ I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/Decoder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using TransponderReceiver;
+
+namespace I4SWTMandatoryExercise2
+{
+    public class Decoder
+    {
+        private ITransponderReceiver receiver;
+        public Decoder(ITransponderReceiver receiver)
+        {
+            this.receiver = receiver;
+
+            // Attach to the event of the real or the fake TDR
+            this.receiver.TransponderDataReady += Decode;
+        }
+
+        public void Decode(object sender, RawTransponderDataEventArgs e)
+        {
+            foreach (var data in e.TransponderData)
+            {
+                FlightData fd = StringToClass(data);
+                System.Console.WriteLine($"Transponderdata:\n ID:{fd.ID}\n x-Coordinate: {fd.xCoordinate}\n y-Coordinate: {fd.yCoordinate}\n z-Coordinate: {fd.zCoordinate}\n Timestamp: {fd.timestamp}");
+            }
+        }
+
+        public FlightData StringToClass(string str)
+        {
+            string[] strList = str.Split(';');
+            FlightData fd = new FlightData(strList[0],
+                Int32.Parse(strList[1]),
+                Int32.Parse(strList[2]),
+                Int32.Parse(strList[3]),
+                DateTime.ParseExact(strList[4], "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            return fd;
+        }
+    }
+
+    public class FlightData
+    {
+        public FlightData(string id, int x, int y, int z, DateTime time)
+        {
+            ID = id;
+            xCoordinate = x;
+            yCoordinate = y;
+            zCoordinate = z;
+            timestamp = time;
+        }
+
+        // Evt. tilføj de resterende data til denne klasse
+
+        public string ID {get; private set; }
+        public int xCoordinate { get; private set; }
+        public int yCoordinate { get; private set; }
+        public int zCoordinate { get; private set; }
+        public DateTime timestamp { get; private set; }
+    }
+}
--- I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/Program.cs
+++ I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TransponderReceiver;
 
 namespace I4SWTMandatoryExercise2
 {
@@ -10,6 +12,15 @@ namespace I4SWTMandatoryExercise2
     {
         static void Main(string[] args)
         {
+            // Using the real transponder data receiver
+            var receiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+
+            // Dependency injection with the real TDR
+            var system = new Decoder(receiver);
+
+            // Let the real TDR execute in the background
+            while (true)
+                Thread.Sleep(1000);
         }
     }
 }
