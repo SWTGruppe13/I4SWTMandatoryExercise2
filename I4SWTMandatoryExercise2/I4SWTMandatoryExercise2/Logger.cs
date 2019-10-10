af941efbc01e50626a5b312269c98e782bac1a7e
Jens Krog Lodberg <jkrog1306@gmail.com>
2019-10-10 15:41:49 +0200

    Added Logger and Renderer. Simple implementation

---
+++ I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace I4SWTMandatoryExercise2
+{
+    public interface ILogger
+    {
+        void Log(FlightData a, FlightData b);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Log(FlightData a, FlightData b)
+        {
+            StreamWriter sw = new StreamWriter("log.txt");
+            sw.WriteLine("Alarm triggered at {0}. Involved planes: {0} and {0}", DateTime.Now.ToString(new CultureInfo("en-GB")), a.ID, b.ID);
+        }
+    }
+}
---
+++ I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/Renderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace I4SWTMandatoryExercise2
+{
+    public class Renderer
+    {
+        public void Display(FlightData fd)
+        {
+            Console.WriteLine("Flight ID: {0}", fd.ID.Substring(0, 6));
+            Console.WriteLine("x-coordinate: {0}", fd.xCoordinate);
+            Console.WriteLine("y-coordinate: {0}", fd.yCoordinate);
+            Console.WriteLine("Altitude: {0}", fd.zCoordinate);
+            //Console.WriteLine("Horizontal Velocity: {0}", fd.Velocity); Velocity not finished
+            Console.WriteLine("Course direction: {0}", fd.CompassCourse);
+        }
+    }
+}
