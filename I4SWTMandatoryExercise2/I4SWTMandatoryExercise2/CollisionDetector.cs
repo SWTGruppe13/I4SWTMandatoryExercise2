0025930647b3ace4964ea0b7a8019be4eea44a3b
Emil Knudsen <eknudsen1992@hotmail.com>
2019-10-23 13:59:05 +0200

    Added collisiondetector

---
+++ I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/CollisionDetector.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace I4SWTMandatoryExercise2
+{
+
+    public interface ICollisionDetector
+    {
+        void DetectCollision(IAirspacePlaneDetector a)
+    }
+
+    public class CollisionDetector : ICollisionDetector
+    {
+        
+    }
+
+
+}
