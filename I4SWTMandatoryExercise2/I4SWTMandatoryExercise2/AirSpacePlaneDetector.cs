571f4deb1ed4717119244fb8cf61be2f087b4f89
Noerlem <Noerlem_m@hotmail.com>
2019-10-23 13:20:27 +0200

    Event added for AirspacePlaneDetector

--- I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/AirSpacePlaneDetector.cs
+++ I4SWTMandatoryExercise2/I4SWTMandatoryExercise2/AirSpacePlaneDetector.cs
@@ -6,16 +6,37 @@ using System.Threading.Tasks;
 
 namespace I4SWTMandatoryExercise2
 {
+    public class PlaneDetectorEventArgs : EventArgs
+    {
+        public PlaneDetectorEventArgs(Point a)
+        {
+            A = a;
+        }
+
+        public Point A;
+    }
+
     class AirSpacePlaneDetector
     {
-        public bool DetectAirplaneInAirspace(Point a, Airspace airspace)
-        {
-            if (a._x > airspace.Center._x + 40000 || a._y > airspace.Center._y + 40000 || a._x < airspace.Center._x - 40000 || a._y < airspace.Center._y - 40000)
-            {
-                return false;
-            }
+        public event EventHandler<PlaneDetectorEventArgs> AirplaneDetected;
 
-            return true; // evebt skal smides
+        AirSpacePlaneDetector(IDecoder de)
+        {
+
+        }
+        public void DetectAirplaneInAirspace(Point a, Airspace airspace)
+        {
+            if (a._x > airspace.Center._x + 40000 || a._y > airspace.Center._y + 40000 ||
+                a._x < airspace.Center._x - 40000 || a._y < airspace.Center._y - 40000)
+            {
+                return;
+            }
+            OnAirplaneDetected(a);
+        }
+
+        protected virtual void OnAirplaneDetected(Point a)
+        {
+            AirplaneDetected?.Invoke(this, new PlaneDetectorEventArgs(a));
         }
     }
 }
