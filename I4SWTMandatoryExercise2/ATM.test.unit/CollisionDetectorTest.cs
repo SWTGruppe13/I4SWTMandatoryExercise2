73271c27c984ca3a919012994941cfc8978f2db4
Emil Knudsen <eknudsen1992@hotmail.com>
2019-10-24 13:59:20 +0200

    CollisionDetectorTest added

--- I4SWTMandatoryExercise2/ATM.test.unit/CollisionDetectorTest.cs
+++ I4SWTMandatoryExercise2/ATM.test.unit/CollisionDetectorTest.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using I4SWTMandatoryExercise2;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ATM.test.unit
 {
+    [TestFixture]
     class CollisionDetectorTest
     {
+        private CollisionDetector _uut;
+        IAirSpacePlaneDetector _airSpacePlaneDetector = Substitute.For<IAirSpacePlaneDetector>();
+        private PlaneDetectorEventArgs receivedArgs;
+        private int NumberOfEvents;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new CollisionDetector(_airSpacePlaneDetector);
+            receivedArgs = null;
+            NumberOfEvents = 0;
+
+            _uut.HandleCollision +=
+                (s, a) =>
+                {
+                    receivedArgs = a;
+                    NumberOfEvents++;
+                };
+        }
     }
 }
