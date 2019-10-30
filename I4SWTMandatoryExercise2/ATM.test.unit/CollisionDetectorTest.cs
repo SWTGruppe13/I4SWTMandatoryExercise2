5f19c36c73def102215f8930af9248035dccf9c0
Emil Knudsen <eknudsen1992@hotmail.com>
2019-10-30 15:03:42 +0100

    CollisionDetectorTest

--- I4SWTMandatoryExercise2/ATM.test.unit/CollisionDetectorTest.cs
+++ I4SWTMandatoryExercise2/ATM.test.unit/CollisionDetectorTest.cs
@@ -13,23 +13,52 @@ namespace ATM.test.unit
     class CollisionDetectorTest
     {
         private CollisionDetector _uut;
-        IAirSpacePlaneDetector _airSpacePlaneDetector = Substitute.For<IAirSpacePlaneDetector>();
-        private PlaneDetectorEventArgs receivedArgs;
+        private CollisionDetectedEventArgs receivedArgs;
         private int NumberOfEvents;
+        private IAirSpacePlaneDetector _fakeAirspacePlaneDetector;
 
-        //[SetUp]
-        //public void Setup()
+        [SetUp]
+        public void Setup()
+        {
+            _fakeAirspacePlaneDetector = Substitute.For<IAirSpacePlaneDetector>();
+            _uut = new CollisionDetector(_fakeAirspacePlaneDetector);
+            receivedArgs = null;
+            NumberOfEvents = 0;
+
+            _uut.CollisionDetectedEvent +=
+                (s, a) =>
+                {
+                    receivedArgs = a;
+                    NumberOfEvents++;
+                };
+        }
+
+        //[Test]
+        //public void PlaneDetectorEvent_RaisedEvent()
         //{
-        //    _uut = new CollisionDetector(_airSpacePlaneDetector);
-        //    receivedArgs = null;
-        //    NumberOfEvents = 0;
-
-        //    _uut.HandleCollision +=
-        //        (s, a) =>
-        //        {
-        //            receivedArgs = a;
-        //            NumberOfEvents++;
-        //        };
+        //    _fakeAirspacePlaneDetector.AirplaneDetected += Raise.EventWith(new object(), new PlaneDetectorEventArgs());
         //}
+
+        [Test]
+        public void CollisionDetectorEvent_RaisedEvent()
+        {
+            FlightData F1 = new FlightData("1");
+            FlightData F2 = new FlightData("2");
+
+            bool wasCalled = false;
+            F1.SetFlightData(8000,8000,8000, new DateTime());
+            F2.SetFlightData(8000,8000,8000, new DateTime());
+
+            List<FlightData> TestPlanes = new List<FlightData>
+            {
+                F1,
+                F2
+            };
+
+            _uut.CollisionDetectedEvent += (o, e) => wasCalled = true;
+            _uut.OnPlaneDetectorEvent(new object(), new PlaneDetectorEventArgs(){PlanesInAirspace = TestPlanes});
+
+            Assert.That(wasCalled, Is.True);
+        }
     }
 }
