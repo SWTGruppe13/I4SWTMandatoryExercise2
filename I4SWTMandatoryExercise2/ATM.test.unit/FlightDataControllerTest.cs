adb317be778580d83c21bbf0a050a716d6b36417
Emil Knudsen <eknudsen1992@hotmail.com>
2019-10-31 13:25:53 +0100

    CollisionDetectorTest virker

--- I4SWTMandatoryExercise2/ATM.test.unit/FlightDataControllerTest.cs
+++ I4SWTMandatoryExercise2/ATM.test.unit/FlightDataControllerTest.cs
@@ -15,10 +15,19 @@ namespace ATM.test.unit
     {
         private FlightDataController _uut;
         private IAirSpacePlaneDetector _fakeAirSpacePlaneDetector;
+        private List<FlightData> _testPlanes;
 
         [SetUp]
         public void Setup()
         {
+            _testPlanes = new List<FlightData>();
+            FlightData F1 = new FlightData("1");
+            FlightData F2 = new FlightData("2");
+            F1.SetFlightData(50,50,500,new DateTime());
+            F2.SetFlightData(50,50,500,new DateTime());
+            _testPlanes.Add(F1);
+            _testPlanes.Add(F2);
+
             _fakeAirSpacePlaneDetector = Substitute.For<IAirSpacePlaneDetector>();
 
         }
@@ -26,6 +35,11 @@ namespace ATM.test.unit
         //[Test] // Test til event
         //public void PlaneDetectorEvent_FlightdataController_RaisedEvent()
         //{
+        //    _fakeAirSpacePlaneDetector.AirplaneDetected += Raise.EventWith<PlaneDetectorEventArgs>(
+        //        this,
+        //        new PlaneDetectorEventArgs(){PlanesInAirspace = _testPlanes});
+
+        //    Assert.That(_uut.ListOrganizer()
         //}
     }
 }
